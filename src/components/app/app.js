43f9d5fa04f66315e47b4848b184a6e94dc28e32
Nematov Muhammadjon <nematovmz2003@gmail.com>
2024-05-04 17:02:57 +0500

    Work with props

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,12 @@ import SearchPanel from '../search-panel/search-panel';
 import './app.css';
 
 function App() {
+  const data = [
+    { name: 'John C.', salary: 800, increase: false },
+    { name: 'Alex M.', salary: 3000, increase: true },
+    { name: 'Carl W.', salary: 5000, increase: false },
+  ];
+
   return (
     <div className="app">
       <AppInfo />
@@ -15,7 +21,7 @@ function App() {
         <AppFilter />
       </div>
 
-      <EmployeesList />
+      <EmployeesList data={data} />
       <EmployeesAddForm />
     </div>
   );
